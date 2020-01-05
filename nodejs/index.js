199d29133f05c615aadc6c486f21a5154c2fa447
vicky1408 <vigneshv01@gmail.com>
2020-01-05 07:55:31 -0500

    Modified index file to facilitate many challenge options

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,5 +1,10 @@
 function challenge1(limit)
 {
+
+    console.log("If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.");
+    console.log("Find the sum of all the multiples of 3 or 5 below " + limit);
+    console.log("\n");
+
     var start=1;
     var result = 0;
     var multiples = [3,5];
@@ -20,8 +25,20 @@ function challenge1(limit)
     }
 
     console.log("Sum of natural numbers below " + limit + " which are multiples of " + multiples + " is " + result);
+    console.log("\n");
 }
 
 
 var arguments = process.argv.slice(2);
-challenge1(arguments[0]);
+
+switch (arguments[0]) {
+    
+    case '1':
+        challenge1(arguments[1]);
+        break;
+
+    default:
+        console.log("Please provide valid input");
+        break;
+
+}
