bb02e7a767d6cd6e14e3215643302a0a270ed05f
vicky1408 <vigneshv01@gmail.com>
2020-01-07 19:42:42 -0500

    Solved Challenge #4

--- nodejs/README.md
+++ nodejs/README.md
@@ -24,3 +24,19 @@ Ex:
 `node index.js 3 23423432`  
 Prime Factors of 23423432 : 2,2,2,449,6521  
 Largest Prime Factor 6521  
+
+**Challenge #4**  
+Run the below in commandline  
+`node index.js [Challenge No] [input number]`  
+
+Ex:  
+`node index.js 4 2`  
+Largest palindrome made from the product of two 2 digit number is '9009'  
+
+**Challenge #5**  
+Run the below in commandline  
+`node index.js [Challenge No] [input number]`  
+
+Ex:  
+`node index.js 5 2`  
+Largest palindrome made from the product of two 2 digit number is '9009'  
--- nodejs/index.js
+++ nodejs/index.js
@@ -112,6 +112,80 @@ function challenge3(intNumber) {
     console.log("Largest Prime Factor " + intlargestPrimeFactor);
 }
 
+function challenge4(intNumber) {
+    console.log(
+        "A palindromic number reads the same both ways. \nThe largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99. \n" +
+        "Find the largest palindrome made from the product of two 3-digit numbers. \n"
+    );
+
+    console.log("solution:");
+    console.log("--------");
+
+    let startNumber = (Math.pow(10, intNumber) - 1);
+    let endNumber = Math.pow(10, (intNumber - 1));
+    let result = 0;
+    let compareValue = 0;
+    let arrResult = [result];
+    let arrPalindrome = [];
+    
+    let doLoop = true;
+    for (let i = startNumber; i >= endNumber; i--)
+    {
+        for (let j = startNumber; j >= endNumber; j--)
+        {
+            result = Math.imul(i, j);
+            arrResult = result.toString().split('');
+            compareValue = parseInt(arrResult.reverse().join(''));
+
+            if (result === compareValue)
+            {
+                arrPalindrome.push(result);
+            }
+        }
+    }
+    
+    console.log("Largest palindrome made from the product of two " + intNumber + " digit number is '" + Math.max(...arrPalindrome) + "'");
+
+}
+
+function challenge5(intNumber) {
+    console.log(
+        "2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. \n" +
+        "What is the smallest positive number that is evenly divisible by all of the numbers from 1 to " + intNumber + "? \n"
+    );
+
+    console.log("solution:");
+    console.log("--------");
+
+    let FoundInteger = false;
+    let result = intNumber;
+
+    while (!FoundInteger)
+    {
+        result++;
+        FoundInteger = true;
+
+        for (let i = 1; i <= intNumber; i++)
+        {
+            if (result % i === 0 && FoundInteger === true)
+            {
+                FoundInteger = true;
+            } else
+            {
+                FoundInteger = false;
+                break;
+            }
+        }
+    }
+
+    if (FoundInteger)
+    {
+        console.log("Smallest Positive number that is evenly divisble by all numbers from 1 to " + intNumber + " is " + result);
+    }
+
+}
+
+
 switch (arguments[0]) {
 
     case '1':
@@ -125,6 +199,14 @@ switch (arguments[0]) {
     case "3":
         challenge3(arguments[1]);
         break;
+
+    case "4":
+        challenge4(arguments[1]);
+        break;        
+
+    case "5":
+        challenge5(arguments[1]);
+        break;    
 
     default:
         console.log("Please provide valid input");
