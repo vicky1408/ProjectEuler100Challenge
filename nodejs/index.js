e153182c74dced07c816e7bf119dec892a864513
vicky1408 <vigneshv01@gmail.com>
2020-01-12 21:41:01 -0500

    Solved Challenge 9

--- nodejs/index.js
+++ nodejs/index.js
@@ -336,6 +336,46 @@ function challenge8(n)
     console.log(numberMax);
 }
 
+function challenge9(n)
+{
+console.log("solution:");
+console.log("--------");
+
+let c = 3;
+
+n = parseInt(n);
+let doLoop = true;
+
+while(doLoop)
+{
+    for(let b = 2; b < c; b++)
+    {
+        for(let a = 2; a < b; a++)
+        {
+            let sqA = Math.pow(a,2);
+            let sqB = Math.pow(b,2);
+            let sqC = Math.pow(c,2);
+            let sqABSum = sqA + sqB;    
+
+            if((sqABSum === sqC) && ((a+b+c) === n))
+            {
+            let sum = a+b+c;
+            console.log("Result " + a+ " " + b + " " +c + " sum: " + sum);
+            console.log("abc " + a*b*c);
+            doLoop = false;
+            break;
+            }
+        }
+
+        if(!doLoop)
+        {
+            break;
+        }
+    }
+    c++;
+}
+console.log(n);
+}
 
 switch (arguments[0]) {
 
@@ -370,7 +410,11 @@ switch (arguments[0]) {
     case "8":
         challenge8(arguments[1]);
         break;
-        
+
+    case "9":
+        challenge9(arguments[1]);
+        break;
+            
     default:
         console.log("Please provide valid input");
         break;
