906d5589bf1618d00ad142814ba92a80da2887b8
vicky1408 <vigneshv01@gmail.com>
2020-01-18 22:12:32 -0500

    updated Challenge 11

--- nodejs/challenge11.js
+++ nodejs/challenge11.js
@@ -1,4 +1,231 @@
-exports.main = function (inputValue) {
-    console.log(inputValue);
-    console.log(Date());
-  }; 
+exports.main = function(n)
+{
+    console.log(
+        "In the 20×20 grid below, four numbers along a diagonal line have been marked in red. \n" +
+        
+        "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 \n"  +
+        "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 \n"  +
+        "81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 \n"  +
+        "52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 \n"  +
+        "22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 \n"  +
+        "24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 \n"  +
+        "32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 \n"  +
+        "67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 \n"  +
+        "24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 \n"  +
+        "21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 \n"  +
+        "78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 \n"  +
+        "16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 \n"  +
+        "86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 \n"  +
+        "19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 \n"  +
+        "04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 \n"  +
+        "88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 \n"  +
+        "04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 \n"  +
+        "20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 \n"  +
+        "20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 \n"  +
+        "01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48 \n"  +
+
+        "The product of these numbers is 26 × 63 × 78 × 14 = 1788696. \n" +
+        "What is the greatest product of " + n + " adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?"
+    );
+
+    console.log("solution:");
+    console.log("--------");
+
+    n = parseInt(n);
+    const inputValue = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
+    const arrNumber = inputValue.toString().split(' ');
+    let arr2d = new Array();
+    let arrTemp = new Array();
+
+    let i = 0, Product1 = 0, Product2 = 0;
+    let numberMax = 0;
+    
+    //n x n Grid
+    const column = n -1;
+    let columnIndex = 0;
+    
+    
+
+    arrNumber.every(function(currentValue) {
+        
+        arrTemp.push(parseInt(currentValue)); 
+        
+        if(columnIndex === column)
+        {
+            arr2d.push(arrTemp);
+            arrTemp = [];
+            columnIndex = 0;
+            // rowIndex++;
+        }
+        else
+        {
+            columnIndex++;
+        }
+
+        return true;
+        
+    });
+
+
+    console.log(arr2d[0].length);
+
+    arr2d.every(function(currentArray, indexArray){
+        
+        currentArray.every(function(currentValue, index){
+            
+            i++;
+
+            if(currentValue > 0)
+            {
+                //Left to Right            
+                if(index < 17)
+                {
+                    Product1 = Math.imul(currentValue, currentArray[index + 1]);
+                    Product2 = Math.imul(currentArray[index + 2], currentArray[index + 3]);
+                    newProduct = Math.imul(Product1, Product2);
+                    // console.log(newProduct);
+                    if(newProduct > numberMax)
+                    {
+                        numberMax = newProduct;
+                    }
+                }
+
+                //Top to Bottom
+                if(indexArray < 17)
+                {
+                    Product1 = Math.imul(currentValue, arr2d[indexArray + 1][index]);
+                    Product2 = Math.imul(arr2d[indexArray + 2][index], arr2d[indexArray + 3][index]);
+                    newProduct = Math.imul(Product1, Product2);
+                    // console.log(newProduct);
+                    if(newProduct > numberMax)
+                    {
+                        numberMax = newProduct;
+                    }
+                }
+                
+                //Diagonal (Left to Right)
+                if(indexArray < 17 && index < 17)
+                {
+                    Product1 = Math.imul(currentValue, arr2d[indexArray + 1][index + 1]);
+                    Product2 = Math.imul(arr2d[indexArray + 2][index + 2], arr2d[indexArray + 3][index + 3]);
+                    newProduct = Math.imul(Product1, Product2);
+                    // console.log(newProduct);
+                    // console.log(indexArray + "," + index + " : " + currentValue );
+                    if(newProduct > numberMax)
+                    {
+                        numberMax = newProduct;
+                    }
+                }
+
+            }
+            return true;
+        });
+        
+
+        return true;
+    });
+
+    console.log(i);
+    console.log(numberMax);
+
+    // arrNumber.every(function(currentValue, index) {
+        
+    //     //Left to Right
+    //     Product1 = Math.imul(currentValue, arrNumber[index + 1]);
+    //     Product2 = Math.imul(arrNumber[index + 2], arrNumber[index + 3]);
+    //     newProduct = Math.imul(Product1, Product2);
+    //     if(newProduct > numberMax)
+    //     {
+    //         numberMax = newProduct;
+    //     }
+
+    //     //Right to Left
+    //     if(index > 3)
+    //     {
+    //         Product1 = Math.imul(currentValue, arrNumber[index - 1]);
+    //         Product2 = Math.imul(arrNumber[index - 2], arrNumber[index - 3]);
+    //         newProduct = Math.imul(Product1, Product2);
+    //         if(newProduct > numberMax)
+    //         {
+    //             numberMax = newProduct;
+    //         }
+    //     }
+
+    //     //Top to Down
+    //     if(index < 17)
+    //     {
+    //         Product1 = Math.imul(currentValue, arrNumber[index + column]);
+    //         Product2 = Math.imul(arrNumber[index + Math.imul(column, 2)], arrNumber[index + Math.imul(column, 3)]);
+    //         newProduct = Math.imul(Product1, Product2);
+    //         if(newProduct > numberMax)
+    //         {
+    //             numberMax = newProduct;
+    //         }
+    //     }
+
+
+    //     //Down to Top
+
+    //     //Diagonal
+
+    //     while(i < 4)
+    //     {
+    //         // arrNumber[]
+    //     }
+    //     i = 0;
+
+
+
+    //     console.log()
+
+    //     // if(currentValue === 0)
+    //     // {
+    //     //     console.log(currentValue);
+    //     // }
+
+    //     return true;
+    // });
+
+    // let multiplierVal = BigInt('1');
+    // let numberMax = BigInt('1');
+
+    // arrNumber.every(function(value, index){
+    //     let i = 1;
+    //     let newVal = value;
+
+    //     if(value == '0')
+    //     {
+    //         return true;
+    //     }
+
+    //     while(i < n)
+    //     {
+    //         if(arrNumber[index+i] == '0')
+    //         {
+    //             newVal = 0;
+    //             break;
+    //         }
+    //         newVal = newVal.concat(arrNumber[index+i]);
+    //         i++;
+    //     }
+        
+    //     if(newVal == '0')
+    //     {
+    //         return true;
+    //     }
+
+        
+    //     newVal.toString().split('').every(function(currentValue) {
+    //         return multiplierVal *= BigInt(currentValue);
+    //     });
+
+    //     if(numberMax < multiplierVal)
+    //     {
+    //         numberMax = multiplierVal;
+    //     }
+    //     multiplierVal = BigInt('1');
+    //    return true;
+    //});
+
+    // console.log(numberMax);
+}
